57df8e00f65b9601433b4052e66bc48dd5c925e9
girish946 <girish946@gmail.com>
2016-10-28 23:32:09 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+setup(
+    name = 'python-R305' ,
+    version = '1.0.0' ,
+    author = 'girish joshi' ,
+    author_email = 'girish946@gmail.com' ,
+    description = ("""python api for R305 finger print module""") ,
+    packages = ['r305'], 
+    install_requires = ['pyserial'], 
+    keywords = 'python api for R305 finger print module' ,
+    scripts = ['Verify.py'], 
+    long_description = """python api for R305 finger print module""",
+    
+  )
