1c91bce48168637dc1f48f5acba2a057159b89b7
girish946 <girish946@gmail.com>
2017-03-01 16:54:33 +0530

    store example added

---
+++ examples/store.py
@@ -0,0 +1,116 @@
+import serial
+import time
+from r305 import genImg, Img2Tz, RegModel, TemplateNum, Store
+import sys
+
+ser = serial.Serial(sys.argv[1])
+
+
+def setBaud():
+    ser.baudrate = 57600
+
+
+def write(callback= None):
+
+    
+    #first finger scan
+    callback("put finger to scan")
+    yield
+    
+    setBaud()    
+    data = genImg.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(ord(c)) for c in s])
+    genImg.parse(s)
+
+    # generate character file of the finger image.
+    callback("generating character file "+ "1")
+    
+
+    data = Img2Tz.getHeader(0x01)
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(ord(c)) for c in s])
+    Img2Tz.parse(s)
+
+
+    #second finger scan.
+    callback("again put finger to scan")
+    yield
+    
+   
+    data = genImg.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(ord(c)) for c in s])
+    genImg.parse(s)
+
+    #generate character file of the finger image.
+    callback("generating character file "+ "1")
+    
+    data = Img2Tz.getHeader(0x02)
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(ord(c)) for c in s])
+    Img2Tz.parse(s)
+
+
+
+    #create RegModel
+    callback("creating reg model")
+    
+    data = RegModel.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(ord(c)) for c in s])
+    RegModel.parse(s)
+    #getTemplateNumber
+    callback("getting template number")
+    
+    data = TemplateNum.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(ord(c)) for c in s])
+    num = TemplateNum.parse(s)
+    callback("template num is "+str(num))
+    print "recived template Number is "+ str(num)
+    
+    
+    data = Store.getHeader(0x01,int(num)+1 )
+    print([hex(c) for c in data])
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(ord(c)) for c in s])
+    print len(s)
+    Store.parse(s)
+
+    #getTemplateNumber
+    data = TemplateNum.getHeader()
+    ser.write(bytearray(data));
+    time.sleep(1)
+    s = ser.read(ser.inWaiting())
+    print([hex(ord(c)) for c in s])
+    num = TemplateNum.parse(s)
+    callback("template stored at "+str(num + 1))
+    print "recived template Number is "+ str(num)
+    return
+    
+    
+    
+def something(msg):
+   print msg
+if __name__ == '__main__':
+    d = write(something)
+    while True:
+        try:
+            d.next()
+        except:
+            break
