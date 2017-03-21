d15202d0a0e8da18a27feda4f4ea44591df2d0e0
girish joshi <girish946@gmail.com>
2017-03-21 17:26:30 +0530

    correction in examples

--- examples/deleteAll.py
+++ examples/deleteAll.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from R305 import DeleteAll
+from r305 import DeleteAll
 import sys
 import serial
 import time
 
 device = sys.argv[1] #this is the serial device (ie R305)
-baudrate = sys,argv[2] #this is the baudrate for the serial communication
+baudrate = sys.argv[2] #this is the baudrate for the serial communication
 
 #generally serial device can be found at /dev/ttyUSBx
 #generally the baudrate for r305 is 57600
@@ -16,7 +16,7 @@ baudrate = sys,argv[2] #this is the baudrate for the serial communication
 ser = serial.Serial(device, baudrate) #init serial
 
 command = DeleteAll.getHeader()     #get the dataframe for the command
-ser.write(bytearray(data));         #the data must be buyte a array
+ser.write(bytearray(command));         #the data must be buyte a array
 time.sleep(1)                       #wait for the device to process command
 response = ser.read(ser.inWaiting())#read all the data that is comming from r305
 print(DeleteAll.parse(response))    #parse the output of the command.
